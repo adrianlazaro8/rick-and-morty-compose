f1774960a62ea011ffc6c95e6b3971135f828ba7
Adrián Lázaro <adri92css@gmail.com>
2022-02-01 19:14:00 +0100

    First commit

---
+++ app/src/main/java/com/adrianlazaro8/rickmorty/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.adrianlazaro8.rickmorty.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
