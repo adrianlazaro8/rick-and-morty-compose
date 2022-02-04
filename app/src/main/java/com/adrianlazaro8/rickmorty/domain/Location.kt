0c6b7cdbf4965ea5ed77d229cd3d14bccb6de470
Adrián Lázaro <adri92css@gmail.com>
2022-02-04 19:38:55 +0100

    feat(domain): Added Episode model

--- app/src/main/java/com/adrianlazaro8/rickmorty/domain/Location.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/domain/Location.kt
@@ -1,6 +1,11 @@
 package com.adrianlazaro8.rickmorty.domain
 
 data class Location(
+    val id: Int,
     val name: String,
-    val url: String
+    val type: String,
+    val dimension: String,
+    val residents: List<String>,
+    val url: String,
+    val created: String
 )
