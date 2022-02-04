d5c7f4d6845facf2705ee3bcd191e561dd4a33bf
Adrián Lázaro <adri92css@gmail.com>
2022-02-04 19:38:55 +0100

    feat(domain): Added Episode model

---
+++ app/src/main/java/com/adrianlazaro8/rickmorty/domain/Episode.kt
@@ -0,0 +1,11 @@
+package com.adrianlazaro8.rickmorty.domain
+
+data class Episode(
+    val id: Int,
+    val name: String,
+    val airDate: String,
+    val episode: String,
+    val characters: List<String>,
+    val url: String,
+    val created: String
+)
