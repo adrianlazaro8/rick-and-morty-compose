98f078237550d5601365767f8d9acf6e34ff8a05
Adrián Lázaro <adri92css@gmail.com>
2022-02-09 17:57:27 +0100

    chore(di): injecting viewModel into MainActivity

--- app/src/main/java/com/adrianlazaro8/rickmorty/MainActivity.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/MainActivity.kt
@@ -3,6 +3,7 @@ package com.adrianlazaro8.rickmorty
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -12,9 +13,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.adrianlazaro8.rickmorty.ui.RickMortyApp
+import com.adrianlazaro8.rickmorty.ui.screens.characters.CharactersViewModel
 import com.adrianlazaro8.rickmorty.ui.theme.RickMortyTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: CharactersViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/characters/CharactersViewModel.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/characters/CharactersViewModel.kt
@@ -1,9 +1,13 @@
 package com.adrianlazaro8.rickmorty.ui.screens.characters
 
 import androidx.lifecycle.ViewModel
+import com.adrianlazaro8.rickmorty.data.CharactersRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class CharactersViewModel @Inject constructor() : ViewModel() {
+class CharactersViewModel @Inject constructor(
+    private val charactersRepository: CharactersRepository
+) : ViewModel() {
+
 }
