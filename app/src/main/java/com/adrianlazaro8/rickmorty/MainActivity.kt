88c920135c53a3c4ef847ef2148b7be69cf58286
Adrián Lázaro <adri92css@gmail.com>
2022-02-11 19:58:59 +0100

    refactor(imports): Removed useless imports

--- app/src/main/java/com/adrianlazaro8/rickmorty/MainActivity.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/MainActivity.kt
@@ -4,17 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.adrianlazaro8.rickmorty.ui.RickMortyApp
 import com.adrianlazaro8.rickmorty.ui.screens.characters.CharactersViewModel
-import com.adrianlazaro8.rickmorty.ui.theme.RickMortyTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
