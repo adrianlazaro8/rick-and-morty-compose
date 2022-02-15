f90414b39ec0c538c4b5f2fdffe597e958b43d07
Adrián Lázaro <adri92css@gmail.com>
2022-02-15 17:36:35 +0100

    fix(imports): removed useless imports

--- app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/characters/CharactersScreen.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/characters/CharactersScreen.kt
@@ -2,25 +2,14 @@ package com.adrianlazaro8.rickmorty.ui.screens.characters
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.GridCells
-import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.adrianlazaro8.rickmorty.R
 import com.adrianlazaro8.rickmorty.domain.Character
-import com.adrianlazaro8.rickmorty.domain.CharacterStatus
-import com.adrianlazaro8.rickmorty.domain.Location
-import com.adrianlazaro8.rickmorty.domain.Origin
 import com.adrianlazaro8.rickmorty.ui.common.CharacterListItem
 import com.adrianlazaro8.rickmorty.ui.common.LazyVerticalGridWithHeader
 
