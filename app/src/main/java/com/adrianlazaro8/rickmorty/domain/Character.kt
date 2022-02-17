8c1f2260daf9056db29b26959e229d9034d6484d
Adrián Lázaro <adri92css@gmail.com>
2022-02-17 20:19:30 +0100

    chore(domain): Character model modified

--- app/src/main/java/com/adrianlazaro8/rickmorty/domain/Character.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/domain/Character.kt
@@ -4,13 +4,10 @@ data class Character(
     val id: Int,
     val name: String,
     val status: CharacterStatus,
-    val species: String,
-    val type: String,
     val gender: String,
     val origin: Origin,
     val location: Location,
     val image: String,
     val episode: List<String>,
-    val url: String,
-    val created: String
+    val url: String
 )
