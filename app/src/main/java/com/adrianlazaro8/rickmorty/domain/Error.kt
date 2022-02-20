25944d65811abd6c089483604db08fb501ef07cd
Adrián Lázaro <adri92css@gmail.com>
2022-02-20 19:48:43 +0100

    chore(errors): mapping errors through extension function

--- app/src/main/java/com/adrianlazaro8/rickmorty/domain/Error.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/domain/Error.kt
@@ -3,3 +3,7 @@ package com.adrianlazaro8.rickmorty.domain
 sealed class Error {
     class Unknown(message: String) : Error()
 }
+
+fun Exception.toError(message: String = ""): Error {
+    return Error.Unknown(message)
+}
