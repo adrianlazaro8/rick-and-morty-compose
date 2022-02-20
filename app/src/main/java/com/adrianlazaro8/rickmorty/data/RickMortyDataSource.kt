381811f9f1d6ccbe69bfe248cbc434a1e0e43abc
Adrián Lázaro <adri92css@gmail.com>
2022-02-20 12:27:40 +0100

    chore(data): handling errors with Either and Error

--- app/src/main/java/com/adrianlazaro8/rickmorty/data/RickMortyDataSource.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/data/RickMortyDataSource.kt
@@ -1,34 +1,42 @@
 package com.adrianlazaro8.rickmorty.data
 
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
 import com.adrianlazaro8.rickmorty.data.dto.CharacterDto
 import com.adrianlazaro8.rickmorty.data.remote.RemoteDataSource
 import com.adrianlazaro8.rickmorty.domain.Episode
 import com.adrianlazaro8.rickmorty.domain.Location
 import com.adrianlazaro8.rickmorty.domain.PaginatedResult
+import com.adrianlazaro8.rickmorty.domain.Error
 
 class RickMortyDataSource(private val rickMortyApi: RickMortyApi) : RemoteDataSource {
 
-    override suspend fun getAllCharacters(): PaginatedResult<List<CharacterDto>> {
-        return rickMortyApi.getAllCharacters()
+    override suspend fun getAllCharacters(): Either<Error, PaginatedResult<List<CharacterDto>>> {
+        return try {
+            rickMortyApi.getAllCharacters().right()
+        } catch (e: Exception) {
+            Error.Unknown(e.toString()).left()
+        }
     }
 
-    override suspend fun getSingleCharacter(id: String): CharacterDto {
+    override suspend fun getSingleCharacter(id: String): Either<Error, CharacterDto> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun getAllLocations(): PaginatedResult<Location> {
+    override suspend fun getAllLocations(): Either<Error, PaginatedResult<Location>> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun getSingleLocation(id: String): Location {
+    override suspend fun getSingleLocation(id: String): Either<Error, Location> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun getAllEpisodes(): PaginatedResult<Episode> {
+    override suspend fun getAllEpisodes(): Either<Error, PaginatedResult<Episode>> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun getSingleEpisode(id: String): Episode {
+    override suspend fun getSingleEpisode(id: String): Either<Error, Episode> {
         TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/adrianlazaro8/rickmorty/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/data/remote/RemoteDataSource.kt
@@ -1,16 +1,18 @@
 package com.adrianlazaro8.rickmorty.data.remote
 
+import arrow.core.Either
 import com.adrianlazaro8.rickmorty.data.dto.CharacterDto
 import com.adrianlazaro8.rickmorty.domain.Episode
 import com.adrianlazaro8.rickmorty.domain.Location
 import com.adrianlazaro8.rickmorty.domain.PaginatedResult
+import com.adrianlazaro8.rickmorty.domain.Error
 
 interface RemoteDataSource {
 
-    suspend fun getAllCharacters(): PaginatedResult<List<CharacterDto>>
-    suspend fun getSingleCharacter(id: String): CharacterDto
-    suspend fun getAllLocations(): PaginatedResult<Location>
-    suspend fun getSingleLocation(id: String): Location
-    suspend fun getAllEpisodes(): PaginatedResult<Episode>
-    suspend fun getSingleEpisode(id: String): Episode
+    suspend fun getAllCharacters(): Either<Error, PaginatedResult<List<CharacterDto>>>
+    suspend fun getSingleCharacter(id: String): Either<Error, CharacterDto>
+    suspend fun getAllLocations(): Either<Error, PaginatedResult<Location>>
+    suspend fun getSingleLocation(id: String): Either<Error, Location>
+    suspend fun getAllEpisodes(): Either<Error, PaginatedResult<Episode>>
+    suspend fun getSingleEpisode(id: String): Either<Error, Episode>
 }
