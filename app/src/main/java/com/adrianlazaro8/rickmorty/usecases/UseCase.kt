60dfbb0e89d023ea0751d38ae1913ee84915bc0c
Adrián Lázaro <adri92css@gmail.com>
2022-02-21 19:46:28 +0100

    chore(errors): UseCase is returning Either

--- app/src/main/java/com/adrianlazaro8/rickmorty/usecases/UseCase.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/usecases/UseCase.kt
@@ -1,5 +1,9 @@
 package com.adrianlazaro8.rickmorty.usecases
 
+import arrow.core.Either
+import com.adrianlazaro8.rickmorty.domain.Error
+
 interface UseCase<T> {
-    suspend fun invoke(): T
+
+    suspend fun invoke(): Either<Error, T>
 }
