6d11375c4d075c714a66b6207f477a0fa5320042
Adrián Lázaro <adri92css@gmail.com>
2022-02-25 19:23:21 +0100

    chore(errors): Returning Either

--- app/src/main/java/com/adrianlazaro8/rickmorty/data/CharactersRepository.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/data/CharactersRepository.kt
@@ -1,16 +1,25 @@
 package com.adrianlazaro8.rickmorty.data
 
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
 import com.adrianlazaro8.rickmorty.data.remote.RemoteDataSource
 import com.adrianlazaro8.rickmorty.domain.Character
+import com.adrianlazaro8.rickmorty.domain.Error
 import com.adrianlazaro8.rickmorty.domain.PaginatedResult
 
 class CharactersRepository(private val remoteDataSource: RemoteDataSource) {
 
-    suspend fun getAllCharacters(): PaginatedResult<List<Character>> {
+    suspend fun getAllCharacters(): Either<Error, PaginatedResult<List<Character>>> {
+
         val response = remoteDataSource.getAllCharacters()
-        return PaginatedResult(
-            response.info,
-            response.results.toDomainCharacterList()
-        )
+        response.fold(ifLeft = {
+            return it.left()
+        }) {
+            return PaginatedResult(
+                it.info,
+                it.results.toDomainCharacterList()
+            ).right()
+        }
     }
 }
--- app/src/main/java/com/adrianlazaro8/rickmorty/usecases/GetAllCharacters.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/usecases/GetAllCharacters.kt
@@ -1,12 +1,14 @@
 package com.adrianlazaro8.rickmorty.usecases
 
+import arrow.core.Either
 import com.adrianlazaro8.rickmorty.data.CharactersRepository
 import com.adrianlazaro8.rickmorty.domain.Character
+import com.adrianlazaro8.rickmorty.domain.Error
 import com.adrianlazaro8.rickmorty.domain.PaginatedResult
 
 class GetAllCharacters(private val charactersRepository: CharactersRepository) :
     UseCase<PaginatedResult<List<Character>>> {
 
-    override suspend fun invoke(): PaginatedResult<List<Character>> =
+    override suspend fun invoke(): Either<Error, PaginatedResult<List<Character>>> =
         charactersRepository.getAllCharacters()
 }
