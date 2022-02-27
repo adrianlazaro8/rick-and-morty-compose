0f1dcba123106904a1a57fa693a78182aef9d358
Adrián Lázaro <adri92css@gmail.com>
2022-02-27 19:55:51 +0100

    chore(errors): New error types

--- app/src/main/java/com/adrianlazaro8/rickmorty/domain/Error.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/domain/Error.kt
@@ -1,9 +1,15 @@
 package com.adrianlazaro8.rickmorty.domain
 
+
 sealed class Error {
+    object Connectivity : Error()
+    class Server(val code: Int) : Error()
     class Unknown(message: String) : Error()
 }
 
 fun Exception.toError(message: String = ""): Error {
+    when (this) {
+        else -> Error.Unknown(message)
+    }
     return Error.Unknown(message)
 }
