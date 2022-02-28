5ea97307199c9b4b49b1873082828d7aabd6d00c
Adrián Lázaro <adri92css@gmail.com>
2022-02-28 20:01:41 +0100

    chore(errors): Showing error view

--- app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/characters/CharactersScreen.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/characters/CharactersScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,6 +15,7 @@ import com.adrianlazaro8.rickmorty.domain.Character
 import com.adrianlazaro8.rickmorty.domain.Error
 import com.adrianlazaro8.rickmorty.domain.PaginatedResult
 import com.adrianlazaro8.rickmorty.ui.common.CharacterListItem
+import com.adrianlazaro8.rickmorty.ui.common.ErrorView
 import com.adrianlazaro8.rickmorty.ui.common.LazyVerticalGridWithHeader
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -32,8 +34,8 @@ fun CharactersScreen(
     }
 
     characters.fold(
-        ifLeft = {
-            //TODO showErrorScreen
+        ifLeft = { error ->
+            ErrorView(error)
         },
         ifRight = {
             it?.let { paginatedCharacters ->
