b7e2b3d102836dfa4d61c1c6e19e12a689327cff
Adrián Lázaro <adri92css@gmail.com>
2022-03-01 18:29:06 +0100

    feat(character detail): Screen for character detail added

---
+++ app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/characterdetail/CharacterDetailScreen.kt
@@ -0,0 +1,9 @@
+package com.adrianlazaro8.rickmorty.ui.screens.characterdetail
+
+import androidx.compose.runtime.Composable
+import com.adrianlazaro8.rickmorty.domain.Character
+
+@Composable
+fun CharacterDetailScreen(character: Character) {
+    
+}
