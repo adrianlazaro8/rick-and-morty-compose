1fae20b152b61d90cb21a02ffd7ecb4f7fc2fca9
Adrián Lázaro <adri92css@gmail.com>
2022-03-04 20:24:51 +0100

    test(CharacterViewModelTest): state value

--- app/src/test/java/com/adrianlazaro8/rickmorty/ui/screens/characters/CharactersViewModelTest.kt
+++ app/src/test/java/com/adrianlazaro8/rickmorty/ui/screens/characters/CharactersViewModelTest.kt
@@ -1,18 +1,57 @@
 package com.adrianlazaro8.rickmorty.ui.screens.characters
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import arrow.core.Either
+import com.adrianlazaro8.rickmorty.domain.Character
+import com.adrianlazaro8.rickmorty.domain.Error
+import com.adrianlazaro8.rickmorty.domain.PaginatedResult
 import com.adrianlazaro8.rickmorty.usecases.GetAllCharacters
+import io.mockk.MockKAnnotations
+import io.mockk.Runs
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.impl.annotations.MockK
-import org.junit.Assert.*
+import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.Assert
 
 import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 
 class CharactersViewModelTest {
 
-    @MockK
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @RelaxedMockK
     lateinit var getAllCharacters: GetAllCharacters
 
+    private lateinit var vm: CharactersViewModel
+
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Before
     fun setUp() {
-        val vm = CharactersViewModel(getAllCharacters)
+        MockKAnnotations.init(this)
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+        vm = CharactersViewModel(getAllCharacters)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `When getAllCharacters is invoked, then data should be returned into state`() = runTest {
+        coEvery { getAllCharacters.invoke() } returns successEither
+
+        vm.getAllCharacters()
+
+        Assert.assertEquals(sucessResult, vm.state)
     }
 }
+
+val successEither: Either<Error, PaginatedResult<List<Character>>?> = Either.Right(null)
+val sucessResult = CharactersViewModel.UiState(false, successEither)
