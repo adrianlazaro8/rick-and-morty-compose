8f9a94eed7b058bf189c070a99bc762df30a92a7
Adrián Lázaro <adri92css@gmail.com>
2022-03-05 12:29:11 +0100

    feat(navigation): Added navigation composable and NavigationItem class

---
+++ app/src/main/java/com/adrianlazaro8/rickmorty/ui/navigation/Navigation.kt
@@ -0,0 +1,23 @@
+package com.adrianlazaro8.rickmorty.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.adrianlazaro8.rickmorty.ui.screens.characters.CharactersScreen
+
+@Composable
+fun Navigation(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = Screens.CHARACTERS.toString()
+    ) {
+        composable(Screens.CHARACTERS.toString()) {
+            CharactersScreen()
+        }
+        composable(Screens.LOCATIONS.toString()) {
+            //TODO
+        }
+    }
+}
---
+++ app/src/main/java/com/adrianlazaro8/rickmorty/ui/navigation/NavigationItem.kt
@@ -0,0 +1,32 @@
+package com.adrianlazaro8.rickmorty.ui.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.ui.graphics.vector.ImageVector
+
+enum class MainNavigationItem(
+    val title: String,
+    val icon: ImageVector,
+    val navigationItem: NavigationItem,
+) {
+
+    CHARACTERS(
+        Screens.CHARACTERS.toString(),
+        Icons.Default.Person,
+        NavigationItem.ContentType(Screens.CHARACTERS)
+    ),
+    LOCATIONS(
+        Screens.LOCATIONS.toString(),
+        Icons.Default.Place,
+        NavigationItem.ContentType(Screens.LOCATIONS)
+    )
+}
+
+sealed class NavigationItem(
+    private val screen: Screens,
+    private val subScreen: String = "main"
+) {
+
+    class ContentType(screen: Screens) : NavigationItem(screen)
+}
