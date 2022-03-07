fdcf4fe252fdcbb7aac88ba302b08b264fef13c7
Adrián Lázaro <adri92css@gmail.com>
2022-03-07 19:07:43 +0100

    test(rick morty data source): Returning locations

--- app/src/main/java/com/adrianlazaro8/rickmorty/data/remote/RickMortyDataSource.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/data/remote/RickMortyDataSource.kt
@@ -5,6 +5,7 @@ import arrow.core.left
 import arrow.core.right
 import com.adrianlazaro8.rickmorty.data.RickMortyApi
 import com.adrianlazaro8.rickmorty.data.dto.CharacterDto
+import com.adrianlazaro8.rickmorty.data.dto.LocationDto
 import com.adrianlazaro8.rickmorty.domain.*
 
 class RickMortyDataSource(private val rickMortyApi: RickMortyApi) : RemoteDataSource {
@@ -21,8 +22,12 @@ class RickMortyDataSource(private val rickMortyApi: RickMortyApi) : RemoteDataSo
         TODO("Not yet implemented")
     }
 
-    override suspend fun getAllLocations(): Either<Error, PaginatedResult<Location>> {
-        TODO("Not yet implemented")
+    override suspend fun getAllLocations(): Either<Error, PaginatedResult<List<LocationDto>>> {
+        return try {
+            rickMortyApi.getAllLocations().right()
+        } catch (e: Exception) {
+            e.toError(e.localizedMessage ?: "").left()
+        }
     }
 
     override suspend fun getSingleLocation(id: String): Either<Error, Location> {
