ff7259033d3a0330c7ba1d38116596d5809088d9
Adrián Lázaro <adri92css@gmail.com>
2022-03-07 19:04:55 +0100

    test(remote data source): New return type for locations

--- app/src/main/java/com/adrianlazaro8/rickmorty/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/data/remote/RemoteDataSource.kt
@@ -2,6 +2,7 @@ package com.adrianlazaro8.rickmorty.data.remote
 
 import arrow.core.Either
 import com.adrianlazaro8.rickmorty.data.dto.CharacterDto
+import com.adrianlazaro8.rickmorty.data.dto.LocationDto
 import com.adrianlazaro8.rickmorty.domain.Episode
 import com.adrianlazaro8.rickmorty.domain.Location
 import com.adrianlazaro8.rickmorty.domain.PaginatedResult
@@ -11,7 +12,7 @@ interface RemoteDataSource {
 
     suspend fun getAllCharacters(): Either<Error, PaginatedResult<List<CharacterDto>>>
     suspend fun getSingleCharacter(id: String): Either<Error, CharacterDto>
-    suspend fun getAllLocations(): Either<Error, PaginatedResult<Location>>
+    suspend fun getAllLocations(): Either<Error, PaginatedResult<List<LocationDto>>>
     suspend fun getSingleLocation(id: String): Either<Error, Location>
     suspend fun getAllEpisodes(): Either<Error, PaginatedResult<Episode>>
     suspend fun getSingleEpisode(id: String): Either<Error, Episode>
