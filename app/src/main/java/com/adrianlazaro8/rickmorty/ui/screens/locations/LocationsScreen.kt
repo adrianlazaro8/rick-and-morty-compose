1b3e8c982ef306b9dc0a9de17b40cbcaa872c983
Adrián Lázaro <adri92css@gmail.com>
2022-03-12 13:52:08 +0100

    feat(ui): Added LocationScreen

--- app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/locations/LocationsScreen.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/locations/LocationsScreen.kt
@@ -1,15 +1,15 @@
 package com.adrianlazaro8.rickmorty.ui.screens.locations
 
 import androidx.compose.runtime.Composable
-import arrow.core.Either
-import com.adrianlazaro8.rickmorty.domain.Error
-import com.adrianlazaro8.rickmorty.domain.Location
-import com.adrianlazaro8.rickmorty.domain.PaginatedResult
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun LocationsScreen(
-    loading: Boolean,
-    characters: Either<Error, PaginatedResult<List<Location>>?>
+    locationsViewModel: LocationsViewModel = hiltViewModel()
 ) {
 
+    LocationsListScreen(
+        locationsViewModel.state.loading,
+        locationsViewModel.state.locations
+    )
 }
