46907b73dda19762a57dd875851dd2a49242a6a8
Adrián Lázaro <adri92css@gmail.com>
2022-03-16 20:42:01 +0100

    chore(di): Providing GetAllLocations use case

--- app/src/main/java/com/adrianlazaro8/rickmorty/di/UsecasesModule.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/di/UsecasesModule.kt
@@ -1,7 +1,9 @@
 package com.adrianlazaro8.rickmorty.di
 
 import com.adrianlazaro8.rickmorty.data.CharactersRepository
+import com.adrianlazaro8.rickmorty.data.LocationsRepository
 import com.adrianlazaro8.rickmorty.usecases.GetAllCharacters
+import com.adrianlazaro8.rickmorty.usecases.GetAllLocations
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,5 +16,9 @@ class UsecasesModule {
     @Provides
     fun provideGetAllCharacter(charactersRepository: CharactersRepository) =
         GetAllCharacters(charactersRepository)
+
+    @Provides
+    fun provideGetAllLocations(locationsRepository: LocationsRepository) =
+        GetAllLocations(locationsRepository)
 
 }
