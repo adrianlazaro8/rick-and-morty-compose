7de3f37f285221bc06a08635d5ef5ff075273c15
Adrián Lázaro <adri92css@gmail.com>
2022-03-22 18:33:53 +0100

    chore(mapper): Added mapper for location

--- app/src/main/java/com/adrianlazaro8/rickmorty/data/Mappers.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/data/Mappers.kt
@@ -1,8 +1,10 @@
 package com.adrianlazaro8.rickmorty.data
 
 import com.adrianlazaro8.rickmorty.data.dto.CharacterDto
+import com.adrianlazaro8.rickmorty.data.dto.EpisodeDto
 import com.adrianlazaro8.rickmorty.data.dto.LocationDto
 import com.adrianlazaro8.rickmorty.domain.Character
+import com.adrianlazaro8.rickmorty.domain.Episode
 import com.adrianlazaro8.rickmorty.domain.Location
 
 fun List<CharacterDto>.toDomainCharacterList() = map { it.toDomainCharacter() }
@@ -31,4 +33,15 @@ fun LocationDto.toDomainLocation(): Location =
         dimension = dimension,
         residents = residents,
         url = url
+    )
+
+fun EpisodeDto.toDomainEpisode() =
+    Episode(
+        id = this.id,
+        name = this.name,
+        airDate = this.airDate,
+        episode = this.episode,
+        characters = this.characters,
+        url = this.url,
+        created = this.created
     )
