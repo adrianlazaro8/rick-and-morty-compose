97aac3316802e67a5a3bd12e4ee65972a187283e
Adrián Lázaro <adri92css@gmail.com>
2022-04-04 20:22:57 +0200

    test(mappers): Added test for mappers

--- app/src/test/java/com/adrianlazaro8/rickmorty/data/MappersTest.kt
+++ app/src/test/java/com/adrianlazaro8/rickmorty/data/MappersTest.kt
@@ -3,9 +3,9 @@ package com.adrianlazaro8.rickmorty.data
 import com.adrianlazaro8.rickmorty.data.dto.CharacterDto
 import com.adrianlazaro8.rickmorty.data.dto.EpisodeDto
 import com.adrianlazaro8.rickmorty.data.dto.LocationDto
-import com.adrianlazaro8.rickmorty.domain.CharacterStatus
-import com.adrianlazaro8.rickmorty.domain.Location
-import com.adrianlazaro8.rickmorty.domain.Origin
+import com.adrianlazaro8.rickmorty.data.dto.OriginDto
+import com.adrianlazaro8.rickmorty.domain.*
+import org.junit.Assert
 import org.junit.Assert.*
 
 import org.junit.Test
@@ -14,15 +14,23 @@ class MappersTest {
 
     @Test
     fun toDomainCharacter() {
+        val domainCharacter = characterDto.toDomainCharacter()
 
+        assertTrue(domainCharacter is Character)
     }
 
     @Test
     fun toDomainLocation() {
+        val domainLocation = locationDto.toDomainLocation()
+
+        assertTrue(domainLocation is Location)
     }
 
     @Test
     fun toDomainEpisode() {
+        val domainEpisode = episodeDto.toDomainEpisode()
+
+        assertTrue(domainEpisode is Episode)
     }
 }
 
@@ -33,8 +41,8 @@ val characterDto = CharacterDto(
     "",
     "",
     "",
-    Origin("", ""),
-    Location(1, "", "", "", listOf(), ""),
+    OriginDto("", ""),
+    LocationDto(1, "", "", "", listOf(), ""),
     "",
     listOf(),
     "",
