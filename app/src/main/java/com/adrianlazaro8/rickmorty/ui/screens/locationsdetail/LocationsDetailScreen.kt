3513981ccd93622af2a5f787fd4ff85e24001d64
Adrián Lázaro <adri92css@gmail.com>
2022-05-20 18:55:15 +0200

    feat(details): Added Location

--- app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/locationsdetail/LocationsDetailScreen.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/locationsdetail/LocationsDetailScreen.kt
@@ -1,8 +1,9 @@
 package com.adrianlazaro8.rickmorty.ui.screens.locationsdetail
 
 import androidx.compose.runtime.Composable
+import com.adrianlazaro8.rickmorty.domain.Location
 
 @Composable
-fun LocationDetailScreen() {
+fun LocationDetailScreen(location: Location) {
 
 }
