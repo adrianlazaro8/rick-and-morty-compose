7798df76a7bd2128f8ec6d98a6b379d85207b9c2
Adrián Lázaro <adri92css@gmail.com>
2022-05-20 18:59:11 +0200

    feat(details): CharactersDetail layout description

--- app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/characterdetail/CharacterDetailScreen.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/characterdetail/CharacterDetailScreen.kt
@@ -1,9 +1,14 @@
 package com.adrianlazaro8.rickmorty.ui.screens.characterdetail
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import com.adrianlazaro8.rickmorty.domain.Character
 
 @Composable
 fun CharacterDetailScreen(character: Character) {
-    
+    Box {
+        //Header nav
+        // Background image
+        //Footer with box containig character info
+    }
 }
