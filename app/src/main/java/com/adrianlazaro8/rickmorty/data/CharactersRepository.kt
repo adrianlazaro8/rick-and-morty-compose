4099acbd9ed3b72bbf1852e639d7248dd74c2ff6
Adrián Lázaro <adri92css@gmail.com>
2022-05-25 21:13:54 +0200

    feat(pagination): Added calling paging into CharactersRepository

--- app/src/main/java/com/adrianlazaro8/rickmorty/data/CharactersRepository.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/data/CharactersRepository.kt
@@ -1,17 +1,21 @@
 package com.adrianlazaro8.rickmorty.data
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import arrow.core.Either
 import arrow.core.left
 import arrow.core.right
+import com.adrianlazaro8.rickmorty.data.remote.CharactersPagingSource
 import com.adrianlazaro8.rickmorty.data.remote.RemoteDataSource
 import com.adrianlazaro8.rickmorty.domain.Character
 import com.adrianlazaro8.rickmorty.domain.Error
 import com.adrianlazaro8.rickmorty.domain.PaginatedResult
+import kotlinx.coroutines.flow.Flow
 
 class CharactersRepository(private val remoteDataSource: RemoteDataSource) {
 
     suspend fun getAllCharacters(): Either<Error, PaginatedResult<List<Character>>> {
-
         val response = remoteDataSource.getAllCharacters()
         response.fold(ifLeft = {
             return it.left()
@@ -21,5 +25,13 @@ class CharactersRepository(private val remoteDataSource: RemoteDataSource) {
                 it.results.toDomainCharacterList()
             ).right()
         }
+    }
+
+    fun getPagedCharacters(): Flow<PagingData<Character>> {
+        return Pager(
+            PagingConfig(pageSize = 20, prefetchDistance = 3)
+        ) {
+            CharactersPagingSource(remoteDataSource)
+        }.flow
     }
 }
