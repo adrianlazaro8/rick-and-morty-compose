fda8b652bb0365deb065b090069d7f4f68c41be8
Adrián Lázaro <adri92css@gmail.com>
2022-05-26 20:37:55 +0200

    feat(pagination): Implementing paging in CharactersListScreen

--- app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/characters/CharactersListScreen.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/characters/CharactersListScreen.kt
@@ -1,48 +1,86 @@
 package com.adrianlazaro8.rickmorty.ui.screens.characters
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import arrow.core.Either
-import com.adrianlazaro8.rickmorty.R
+import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
+import androidx.paging.PagingData
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.adrianlazaro8.rickmorty.domain.Character
-import com.adrianlazaro8.rickmorty.domain.Error
-import com.adrianlazaro8.rickmorty.domain.PaginatedResult
 import com.adrianlazaro8.rickmorty.ui.common.CharacterListItem
-import com.adrianlazaro8.rickmorty.ui.common.CircularProgressBarView
-import com.adrianlazaro8.rickmorty.ui.common.ErrorView
-import com.adrianlazaro8.rickmorty.ui.common.LazyVerticalGridWithHeader
+import kotlinx.coroutines.flow.Flow
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun CharactersListScreen(
-    loading: Boolean,
-    characters: Either<Error, PaginatedResult<List<Character>>?>
+    paginatedCharacters: Flow<PagingData<Character>>
 ) {
-    if (loading) {
-        CircularProgressBarView()
-    }
 
-    characters.fold(
-        ifLeft = { error ->
-            ErrorView(error)
-        },
-        ifRight = {
-            it?.let { paginatedCharacters ->
-                if (paginatedCharacters.results.isNotEmpty()) {
-                    LazyVerticalGridWithHeader(
-                        title = stringResource(id = R.string.characters),
-                        count = paginatedCharacters.results.count(),
-                        2,
-                        gridItem = { CharacterListItem(character = paginatedCharacters.results[it]) })
+    val charactersList = paginatedCharacters.collectAsLazyPagingItems()
+
+    LazyVerticalGrid(
+        modifier = Modifier.padding(start = 6.dp, end = 6.dp),
+        cells = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(4.dp)
+    )
+    {
+        items(charactersList.itemCount) { index ->
+            charactersList[index]?.let {
+                CharacterListItem(character = it)
+            }
+        }
+        charactersList.apply {
+            when {
+                loadState.refresh is LoadState.Loading -> {
+                    //You can add modifier to manage load state when first time response page is loading
+                }
+                loadState.append is LoadState.Loading -> {
+                    //You can add modifier to manage load state when next response page is loading
+                }
+                loadState.append is LoadState.Error -> {
+                    //You can use modifier to show error message
                 }
             }
         }
-    )
+    }
 }
+
+//    LazyColumn {
+//        items(charactersList){ item ->
+//            item?.let {
+//                LazyVerticalGridWithHeader(
+//                    title = stringResource(id = R.string.characters),
+//                    count = charactersList.itemCount,
+//                    2,
+//                    gridItem = { CharacterListItem(character = paginatedCharacters.results[it]) })
+//            }
+//        }
+//}
+
+//    if (loading) {
+//        CircularProgressBarView()
+//    }
+//
+//    characters.fold(
+//        ifLeft = { error ->
+//            ErrorView(error)
+//        },
+//        ifRight = {
+//            it?.let { paginatedCharacters ->
+//                if (paginatedCharacters.results.isNotEmpty()) {
+//                    LazyVerticalGridWithHeader(
+//                        title = stringResource(id = R.string.characters),
+//                        count = paginatedCharacters.results.count(),
+//                        2,
+//                        gridItem = { CharacterListItem(character = paginatedCharacters.results[it]) })
+//                }
+//            }
+//        }
+//    )
+//}
