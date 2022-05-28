9c49d13011c42493ce32205b8a9eb9b65768e905
Adrián Lázaro <adri92css@gmail.com>
2022-05-28 13:03:52 +0200

    feat(navigation): Getting id into character detail

--- app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/characterdetail/CharacterDetailScreen.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/characterdetail/CharacterDetailScreen.kt
@@ -1,12 +1,14 @@
 package com.adrianlazaro8.rickmorty.ui.screens.characterdetail
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import com.adrianlazaro8.rickmorty.domain.Character
 
 @Composable
-fun CharacterDetailScreen(character: Character) {
+fun CharacterDetailScreen(id: String) {
     Box {
+        Text(text = id)
         //Header nav
         // Background image
         //Footer with box containig character info
