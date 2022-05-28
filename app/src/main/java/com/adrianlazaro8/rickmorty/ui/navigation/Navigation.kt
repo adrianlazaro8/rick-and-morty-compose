027a8110709729a6ac38c10d0b32a766044d5dda
Adrián Lázaro <adri92css@gmail.com>
2022-05-28 13:03:29 +0200

    feat(navigation): Character detail

--- app/src/main/java/com/adrianlazaro8/rickmorty/ui/navigation/Navigation.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/ui/navigation/Navigation.kt
@@ -2,9 +2,11 @@ package com.adrianlazaro8.rickmorty.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.adrianlazaro8.rickmorty.ui.screens.characterdetail.CharacterDetailScreen
 import com.adrianlazaro8.rickmorty.ui.screens.characters.CharactersScreen
 import com.adrianlazaro8.rickmorty.ui.screens.locations.LocationsScreen
 
@@ -12,12 +14,24 @@ import com.adrianlazaro8.rickmorty.ui.screens.locations.LocationsScreen
 fun Navigation(navController: NavHostController) {
     NavHost(
         navController = navController,
-        startDestination = Screens.CHARACTERS.toString()
+        startDestination = Screens.CHARACTERS.route
     ) {
-        composable(Screens.CHARACTERS.toString()) {
-            CharactersScreen()
+        composable(Screens.CHARACTERS.route) {
+            CharactersScreen {
+                navController.navigate(
+                    Screens.CHARACTERS_DETAIL.route.replace(
+                        "{id}",
+                        it.id.toString()
+                    )
+                )
+            }
         }
-        composable(Screens.LOCATIONS.toString()) {
+        composable(Screens.CHARACTERS_DETAIL.route,
+            arguments = listOf(navArgument("id") { type = NavType.StringType })
+        ) {
+            CharacterDetailScreen(id = it.arguments?.getString("id") ?: "")
+        }
+        composable(Screens.LOCATIONS.route) {
             LocationsScreen()
         }
     }
