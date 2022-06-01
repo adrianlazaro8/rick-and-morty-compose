b1210b92e1fd20db9e866b4728b5b52a3f3161c6
Adrián Lázaro <adri92css@gmail.com>
2022-06-01 21:10:56 +0200

    feat(styles): Color for bottom bar

--- app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/characters/CharactersScreen.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/characters/CharactersScreen.kt
@@ -1,30 +1,21 @@
 package com.adrianlazaro8.rickmorty.ui.screens.characters
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
-import arrow.core.Either
-import com.adrianlazaro8.rickmorty.R
 import com.adrianlazaro8.rickmorty.domain.Character
-import com.adrianlazaro8.rickmorty.domain.Error
-import com.adrianlazaro8.rickmorty.domain.PaginatedResult
-import com.adrianlazaro8.rickmorty.ui.common.CharacterListItem
-import com.adrianlazaro8.rickmorty.ui.common.ErrorView
-import com.adrianlazaro8.rickmorty.ui.common.LazyVerticalGridWithHeader
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun CharactersScreen(
-    charactersViewModel: CharactersViewModel = hiltViewModel()
+    charactersViewModel: CharactersViewModel = hiltViewModel(),
+    onClick: (Character) -> Unit
+
 ) {
     CharactersListScreen(
-        charactersViewModel.paginatedCharacters
+        charactersViewModel.paginatedCharacters,
+        onClick = {
+            onClick(it)
+        }
     )
 }
