4139ddfffda0715fc10de1fd418f646109cf360c
Adrián Lázaro <adri92css@gmail.com>
2022-06-01 21:10:50 +0200

    feat(pagination): Implementation

--- app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/characters/CharactersListScreen.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/characters/CharactersListScreen.kt
@@ -2,16 +2,20 @@ package com.adrianlazaro8.rickmorty.ui.screens.characters
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
 import androidx.paging.PagingData
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.adrianlazaro8.rickmorty.R
 import com.adrianlazaro8.rickmorty.domain.Character
 import com.adrianlazaro8.rickmorty.ui.common.CharacterListItem
 import kotlinx.coroutines.flow.Flow
@@ -19,36 +23,49 @@ import kotlinx.coroutines.flow.Flow
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun CharactersListScreen(
-    paginatedCharacters: Flow<PagingData<Character>>
+    paginatedCharacters: Flow<PagingData<Character>>,
+    onClick: (Character) -> Unit
 ) {
 
     val charactersList = paginatedCharacters.collectAsLazyPagingItems()
 
-    LazyVerticalGrid(
-        modifier = Modifier.padding(start = 6.dp, end = 6.dp),
-        cells = GridCells.Fixed(2),
-        verticalArrangement = Arrangement.spacedBy(4.dp)
-    )
-    {
-        items(charactersList.itemCount) { index ->
-            charactersList[index]?.let {
-                CharacterListItem(character = it)
+    Column(
+        modifier = Modifier.padding(start = 6.dp, end = 6.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.characters),
+            style = MaterialTheme.typography.h5,
+            color = MaterialTheme.colors.primary
+        )
+        LazyVerticalGrid(
+            cells = GridCells.Fixed(2),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        )
+        {
+            items(charactersList.itemCount) { index ->
+                charactersList[index]?.let {
+                    CharacterListItem(
+                        character = it,
+                        onClick
+                    )
+                }
             }
-        }
-        charactersList.apply {
-            when {
-                loadState.refresh is LoadState.Loading -> {
-                    //You can add modifier to manage load state when first time response page is loading
-                }
-                loadState.append is LoadState.Loading -> {
-                    //You can add modifier to manage load state when next response page is loading
-                }
-                loadState.append is LoadState.Error -> {
-                    //You can use modifier to show error message
+            charactersList.apply {
+                when {
+                    loadState.refresh is LoadState.Loading -> {
+                        //You can add modifier to manage load state when first time response page is loading
+                    }
+                    loadState.append is LoadState.Loading -> {
+                        //You can add modifier to manage load state when next response page is loading
+                    }
+                    loadState.append is LoadState.Error -> {
+                        //You can use modifier to show error message
+                    }
                 }
             }
         }
     }
+
 }
 
 //    LazyColumn {
