2feae079cf5ee07d2f23384d3a103f3bee38f0d3
Adrián Lázaro <adri92css@gmail.com>
2022-06-08 21:05:26 +0200

    feat(Character Detail): Implementing ViewModel

--- app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/characterdetail/CharacterDetailViewModel.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/characterdetail/CharacterDetailViewModel.kt
@@ -4,7 +4,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import com.adrianlazaro8.rickmorty.ui.screens.characters.CharactersViewModel
+import arrow.core.Either
+import com.adrianlazaro8.rickmorty.domain.Character
+import com.adrianlazaro8.rickmorty.domain.Error
 import com.adrianlazaro8.rickmorty.usecases.GetAllCharacters
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -14,6 +16,11 @@ class CharacterDetailViewModel @Inject constructor(
     private val getAllCharacters: GetAllCharacters,
 ) : ViewModel() {
 
-    var state by mutableStateOf(CharactersViewModel.UiState())
+    var state by mutableStateOf(UiState())
         private set
+
+    data class UiState(
+        val loading: Boolean = false,
+        val characters: Either<Error, Character?> = Either.Right(null)
+    )
 }
