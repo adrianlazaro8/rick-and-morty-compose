9098832299978a4024611778baaf0f8094ef715d
Adrián Lázaro <adri92css@gmail.com>
2022-06-11 09:46:33 +0200

    feat(ui): Showing image in CharacterDetail

--- app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/characterdetail/CharacterDetailItemView.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/characterdetail/CharacterDetailItemView.kt
@@ -1,9 +1,19 @@
 package com.adrianlazaro8.rickmorty.ui.screens.characterdetail
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import arrow.core.Either
+import coil.compose.rememberImagePainter
 import com.adrianlazaro8.rickmorty.domain.Character
 import com.adrianlazaro8.rickmorty.domain.Error
 
@@ -24,7 +34,20 @@ fun CharacterDetailItemView(
         ifRight = {
             it?.let { character ->
                 Box {
-                    Text(text = character.name)
+                    Card(
+                        modifier = Modifier
+                            .height(200.dp)
+                            .padding(4.dp)
+                            .fillMaxWidth()
+                    ) {
+                        Image(
+                            painter = rememberImagePainter(data = character.image),
+                            contentDescription = character.name,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        )
+                    }
                     //Header nav
                     // Background image
                     //Footer with box containig character info
