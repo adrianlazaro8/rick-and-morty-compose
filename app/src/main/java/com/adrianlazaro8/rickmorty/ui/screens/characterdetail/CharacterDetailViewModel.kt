a55b348cc9d90d3f8ebe9e8e298426469bea5e6c
Adrián Lázaro <adri92css@gmail.com>
2022-06-15 18:07:26 +0200

    refactor(improvements): Using SavedStateHandle from ViewModel

--- app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/characterdetail/CharacterDetailViewModel.kt
+++ app/src/main/java/com/adrianlazaro8/rickmorty/ui/screens/characterdetail/CharacterDetailViewModel.kt
@@ -3,21 +3,41 @@ package com.adrianlazaro8.rickmorty.ui.screens.characterdetail
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import arrow.core.Either
 import com.adrianlazaro8.rickmorty.domain.Character
 import com.adrianlazaro8.rickmorty.domain.Error
+import com.adrianlazaro8.rickmorty.ui.screens.characters.CharactersViewModel
 import com.adrianlazaro8.rickmorty.usecases.GetAllCharacters
+import com.adrianlazaro8.rickmorty.usecases.GetSingleCharacter
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CharacterDetailViewModel @Inject constructor(
-    private val getAllCharacters: GetAllCharacters,
+    savedStateHandle: SavedStateHandle,
+    private val getSingleCharacter: GetSingleCharacter
 ) : ViewModel() {
+
+    private val characterId = savedStateHandle.get<String>("id")
+        ?: throw IllegalAccessException("CharacterId is required")
 
     var state by mutableStateOf(UiState())
         private set
+
+    init {
+        getSingleCharacter(characterId)
+    }
+
+    private fun getSingleCharacter(characterId: String) {
+        viewModelScope.launch {
+            state = UiState(loading = true)
+            state = UiState(characters = getSingleCharacter.invoke(characterId))
+        }
+    }
 
     data class UiState(
         val loading: Boolean = false,
